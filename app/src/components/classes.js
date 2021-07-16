c64604638f1a415a7fc3a254349d0261d42beda6
Vivianne Yee <yee.vivianne@gmail.com>
2021-07-16 09:19:03 -0400

    create classes section

--- app/src/components/classes.js
+++ app/src/components/classes.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import {Card, Button} from 'react-bootstrap'
+import {Card, Button, Modal} from 'react-bootstrap'
 import './classes.css'
 import YogaClass from './images/yoga_class.jpeg'
+import CalendarIcon from './images/calendar_icon.png'
 
 export default class Classes extends React.Component {
   render() {
@@ -10,17 +11,39 @@ export default class Classes extends React.Component {
         <h1 className="pageTitle">Fitness Classes</h1>
         <div>
           <div className="row">
-            <Card className="left-col">
-              <Card.Img variant="top" src={YogaClass} className="classImage"/>
-              <Card.Body>
-                <Card.Title>Yoga</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
+            <div className="left-col">
+              <Card className="class-card">
+                <Card.Img variant="top" src={YogaClass} className="classImage"/>
+                <Card.Body>
+                  <Card.Title>Yoga</Card.Title>
+                  <Card.Text>
+                    Make yoga and meditation part of your daily life.
+                  </Card.Text>
+                  <div className="schedule">
+                    <img src={CalendarIcon}/>
+                    <span>See Schedule</span>
+                  </div>
+                  <Button className="registerBtn">Register</Button>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="right-col">
+              <Card className="class-card">
+                <Card.Img variant="top" src={YogaClass} className="classImage"/>
+                <Card.Body>
+                  <Card.Title>Yoga</Card.Title>
+                  <Card.Text>
+                    Make yoga and meditation part of your daily life.
+                  </Card.Text>
+                  <div className="schedule">
+                    <img src={CalendarIcon}/>
+                    <span>See Schedule</span>
+                  </div>
+                  <Button className="registerBtn">Register</Button>
+                </Card.Body>
+              </Card>
+            </div>
           </div>
         </div>
       </div>
